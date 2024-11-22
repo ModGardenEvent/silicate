8b24edf2721cd6415c1e18809f8eba394566fa14
sylv256 <contact@sylv.gay>
2024-11-22 16:45:41 -0500

    docs: add See Also for GameConditionType

--- common/src/main/java/house/greenhouse/silicate/api/condition/GameConditionType.java
+++ common/src/main/java/house/greenhouse/silicate/api/condition/GameConditionType.java
@@ -1,12 +1,14 @@
 package house.greenhouse.silicate.api.condition;
 
 import com.mojang.serialization.MapCodec;
+import house.greenhouse.silicate.api.SilicateRegistries;
 import org.jetbrains.annotations.ApiStatus;
 
 /**
  * A utility class used in codec definition for creating {@link GameCondition}s (subclasses).
  * @param codec The {@link MapCodec} of the {@link GameCondition}, returned by {@link GameCondition#getCodec()}.
  * @param <T> The type of the {@link GameCondition} subclass.
+ * @see SilicateRegistries#GAME_CONDITION_TYPE
  */
 @ApiStatus.Experimental
 public record GameConditionType<T extends GameCondition<T>>(MapCodec<T> codec) {
