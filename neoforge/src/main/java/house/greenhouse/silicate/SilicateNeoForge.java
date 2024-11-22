934a61e5b32443a45606e2092e42a4bf5df80216
sylv256 <contact@sylv.gay>
2024-11-21 20:39:03 -0500

    fix: set helper before initialization

--- neoforge/src/main/java/house/greenhouse/silicate/SilicateNeoForge.java
+++ neoforge/src/main/java/house/greenhouse/silicate/SilicateNeoForge.java
@@ -6,11 +6,13 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import org.jetbrains.annotations.ApiStatus;
 
+import static house.greenhouse.silicate.Silicate.MOD_ID;
+
 @ApiStatus.Internal
-@Mod(Silicate.MOD_ID)
+@Mod(MOD_ID)
 public class SilicateNeoForge {
 	public SilicateNeoForge(IEventBus eventBus) {
-		Silicate.init();
 		Silicate.setHelper(new SilicatePlatformHelperNeoForge());
+		Silicate.init();
 	}
 }
