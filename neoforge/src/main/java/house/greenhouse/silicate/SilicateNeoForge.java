d7cff24957cabc6f9303576de8a14bd1ca44a120
sylv256 <contact@sylv.gay>
2024-11-22 21:24:31 -0500

    fix: Stop registering GameTests the proper way because that didn't work.

--- neoforge/src/main/java/house/greenhouse/silicate/SilicateNeoForge.java
+++ neoforge/src/main/java/house/greenhouse/silicate/SilicateNeoForge.java
@@ -2,29 +2,24 @@ package house.greenhouse.silicate;
 
 
 import house.greenhouse.silicate.platform.SilicatePlatformHelperNeoForge;
+import net.minecraft.gametest.framework.GameTestRegistry;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.event.RegisterGameTestsEvent;
 import org.jetbrains.annotations.ApiStatus;
 
 import static house.greenhouse.silicate.Silicate.MOD_ID;
 
 @ApiStatus.Internal
-@EventBusSubscriber(
-	modid = MOD_ID,
-	bus = EventBusSubscriber.Bus.MOD
-)
 @Mod(MOD_ID)
 public class SilicateNeoForge {
 	public SilicateNeoForge(IEventBus eventBus) {
 		Silicate.setHelper(new SilicatePlatformHelperNeoForge());
+		Silicate.GAME_TESTS.forEach(SilicateNeoForge::registerGameTest);
 		Silicate.init();
 	}
 	
-	@SubscribeEvent
-	public static void registerTests(RegisterGameTestsEvent event) {
-		Silicate.GAME_TESTS.forEach(event::register);
+	@SuppressWarnings("deprecation") // We don't care about Neo's warnings.
+	private static void registerGameTest(Class<?> clazz) {
+		GameTestRegistry.register(clazz);
 	}
 }
