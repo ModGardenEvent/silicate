abf01777cd8363ceccce3f0fdc529ac98989e9f7
sylv256 <contact@sylv.gay>
2024-11-22 20:51:10 -0500

    fix: Register GameTests the proper way on NeoForge

--- common/src/main/java/house/greenhouse/silicate/Silicate.java
+++ common/src/main/java/house/greenhouse/silicate/Silicate.java
@@ -1,28 +1,27 @@
 package house.greenhouse.silicate;
 
-import house.greenhouse.silicate.platform.Platform;
 import house.greenhouse.silicate.platform.SilicatePlatformHelper;
 import house.greenhouse.silicate.test.SilicateGameTests;
-import net.minecraft.gametest.framework.GameTestRegistry;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.ApiStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 @ApiStatus.Internal
 public class Silicate {
 	public static final String MOD_ID = "silicate";
 	public static final String MOD_NAME = "Silicate";
 	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+	public static final List<Class<?>> GAME_TESTS = List.of(
+		SilicateGameTests.class
+	);
 
 	private static SilicatePlatformHelper helper;
 
 	public static void init() {
 		LOG.info("Initializing Silicate");
-		// NeoForge f&#!ery Workaround
-		if (!helper.getPlatform().equals(Platform.FABRIC)) {
-			GameTestRegistry.register(SilicateGameTests.class);
-		}
 	}
 	
 	public static SilicatePlatformHelper getHelper() {
