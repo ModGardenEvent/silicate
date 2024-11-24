338dd65cd28f7356726b95d18b8226e821cc5b58
sylv256 <contact@sylv.gay>
2024-11-23 22:59:59 -0500

    refactor: remove `@ApiStatus.Experimental` from `GameConditionType`

--- common/src/main/java/house/greenhouse/silicate/api/condition/GameConditionType.java
+++ common/src/main/java/house/greenhouse/silicate/api/condition/GameConditionType.java
@@ -2,7 +2,6 @@ package house.greenhouse.silicate.api.condition;
 
 import com.mojang.serialization.MapCodec;
 import house.greenhouse.silicate.api.SilicateRegistries;
-import org.jetbrains.annotations.ApiStatus;
 
 /**
  * A utility class used in codec definition for creating {@link GameCondition}s (subclasses).
@@ -10,6 +9,5 @@ import org.jetbrains.annotations.ApiStatus;
  * @param <T> The type of the {@link GameCondition} subclass.
  * @see SilicateRegistries#GAME_CONDITION_TYPE
  */
-@ApiStatus.Experimental
 public record GameConditionType<T extends GameCondition<T>>(MapCodec<T> codec) {
 }
