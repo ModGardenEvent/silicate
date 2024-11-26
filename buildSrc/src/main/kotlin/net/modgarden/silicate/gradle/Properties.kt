7ea25344e9e53e24b0436374cb6aaebe1747e1df
sylv256 <contact@sylv.gay>
2024-11-26 18:56:49 -0500

    fix: use correct Modrinth project ID

--- buildSrc/src/main/kotlin/net/modgarden/silicate/gradle/Properties.kt
+++ buildSrc/src/main/kotlin/net/modgarden/silicate/gradle/Properties.kt
@@ -11,7 +11,7 @@ object Properties {
 	const val LICENSE = "MPL-2.0"
 
 	const val MODRINTH_PAGE = "https://modrinth.com/mod/silicate"
-	const val MODRINTH_PROJECT_ID = "????????"
+	const val MODRINTH_PROJECT_ID = "vaQ7oWvL"
 	const val GITHUB_REPO = "ModGardenEvent/silicate"
 	const val GITHUB_COMMITISH = Versions.MINECRAFT
 }
