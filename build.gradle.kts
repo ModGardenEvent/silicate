793f2feccc1a0a2eef54998e08a6502621f14ac9
sylv256 <contact@sylv.gay>
2025-02-09 18:21:43 -0500

    [no ci] ci: bump mod-publish-plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
 	id("fabric-loom") version "1.7-SNAPSHOT" apply false
-	id("net.neoforged.moddev") version "2.0.27-beta" apply false
-	id("me.modmuss50.mod-publish-plugin") version "0.6.2" apply false
+	id("net.neoforged.moddev") version "2.0.76" apply false
+	id("me.modmuss50.mod-publish-plugin") version net.modgarden.silicate.gradle.Versions.MOD_PUBLISH_PLUGIN apply false
 }
