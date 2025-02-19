f245b7d99451354b759a155ef78f6fe26b411068
MerchantPug <merchantpug@gmail.com>
2025-02-20 08:01:20 +1100

    chore: Re-update dependency versions.

--- buildSrc/src/main/kotlin/net/modgarden/silicate/gradle/Versions.kt
+++ buildSrc/src/main/kotlin/net/modgarden/silicate/gradle/Versions.kt
@@ -9,7 +9,7 @@ object Versions {
 	const val PARCHMENT = "2024.07.28"
 
 	const val FABRIC_API = "0.106.0+1.21.1"
-	const val FABRIC_LOADER = "0.15.11"
+	const val FABRIC_LOADER = "0.16.10"
 	const val MOD_MENU = "11.0.3"
 	const val FABRIC_LOADER_RANGE = ">=0.15"
 	const val FABRIC_MINECRAFT_RANGE = ">=1.21"
@@ -18,8 +18,8 @@ object Versions {
 	const val NEOFORGE_LOADER_RANGE = "[4,)"
 	const val NEOFORGE_MINECRAFT_RANGE = "[1.21,)"
 
-	const val FABRIC_MIXIN = "0.15.0+mixin.0.8.7"
-	const val MIXIN_EXTRAS = "0.4.0"
+	const val FABRIC_MIXIN = "0.15.5+mixin.0.8.7"
+	const val MIXIN_EXTRAS = "0.4.1"
 
 	const val MOD_PUBLISH_PLUGIN = "0.8.4"
 
