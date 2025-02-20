36b1f5c8af25bd67a8abd83ad7407a82f644423c
MerchantPug <merchantpug@gmail.com>
2025-02-20 12:04:43 +1100

    build: Fix Minecraft version range to 1.21/1.21.1.

--- buildSrc/src/main/kotlin/net/modgarden/silicate/gradle/Versions.kt
+++ buildSrc/src/main/kotlin/net/modgarden/silicate/gradle/Versions.kt
@@ -12,11 +12,11 @@ object Versions {
 	const val FABRIC_LOADER = "0.16.10"
 	const val MOD_MENU = "11.0.3"
 	const val FABRIC_LOADER_RANGE = ">=0.15"
-	const val FABRIC_MINECRAFT_RANGE = ">=1.21"
+	const val FABRIC_MINECRAFT_RANGE = ">=1.21 <=1.21.1"
 
 	const val NEOFORGE = "21.1.73"
 	const val NEOFORGE_LOADER_RANGE = "[4,)"
-	const val NEOFORGE_MINECRAFT_RANGE = "[1.21,)"
+	const val NEOFORGE_MINECRAFT_RANGE = "[1.21,1.21.2)"
 
 	const val FABRIC_MIXIN = "0.15.5+mixin.0.8.7"
 	const val MIXIN_EXTRAS = "0.4.1"
