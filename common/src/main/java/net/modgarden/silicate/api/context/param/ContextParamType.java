a7510783980b8c522ab6d7eb49ae07f661107069
MerchantPug <merchantpug@gmail.com>
2025-02-20 11:37:30 +1100

    fix: Javadoc errors in ContextParamType.

--- common/src/main/java/net/modgarden/silicate/api/context/param/ContextParamType.java
+++ common/src/main/java/net/modgarden/silicate/api/context/param/ContextParamType.java
@@ -2,9 +2,9 @@ package net.modgarden.silicate.api.context.param;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
+import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceLocation;
 import net.modgarden.silicate.api.SilicateBuiltInRegistries;
-import net.modgarden.silicate.api.condition.GameCondition;
 import net.modgarden.silicate.api.condition.TypedGameCondition;
 import org.jetbrains.annotations.NotNull;
 
@@ -29,8 +29,8 @@ public record ContextParamType<T>(ResourceLocation name, Class<T> clazz) {
 	 * @param clazz The class of the type in {@link T}.
 	 * @return The parameter type's codec.
 	 * @param <T> The value type of the parameter type.
-	 * @implNote Although potentially unsafe, this shouldn't result in any Mad Gadget situations given that the actual non-erased type at runtime will be checked. See the implementation of {@link TypedGameCondition#validate(GameCondition, Class)} for further information.
-	 * @see TypedGameCondition#validate(GameCondition, Class)
+	 * @implNote Although potentially unsafe, this shouldn't result in any Mad Gadget situations given that the actual non-erased type at runtime will be checked. See the implementation of {@link TypedGameCondition#validate(Holder, Class)} for further information.
+	 * @see TypedGameCondition#validate(Holder, Class)
 	 */
 	@SuppressWarnings("JavadocReference") // We want people to be able to verify the underlying implementation.
 	public static <T> Codec<ContextParamType<T>> getCodec(Class<T> clazz) {
