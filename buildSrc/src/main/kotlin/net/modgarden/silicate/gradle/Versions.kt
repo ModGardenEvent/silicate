af7411cc4f7f6e60550ff1094e48a3dad0143089
sylv256 <contact@sylv.gay>
2025-03-24 20:23:09 -0400

    chore: bump version

--- buildSrc/src/main/kotlin/net/modgarden/silicate/gradle/Versions.kt
+++ buildSrc/src/main/kotlin/net/modgarden/silicate/gradle/Versions.kt
@@ -1,7 +1,7 @@
 package net.modgarden.silicate.gradle
 
 object Versions {
-	const val MOD = "0.7.1"
+	const val MOD = "0.8.0"
 
 	const val MINECRAFT = "1.21.1"
 	const val PARCHMENT_MINECRAFT = "1.21"
