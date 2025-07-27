0522b63b4f802858d3c3ac8e5bcca329d8784465
Anastasia Calico <calico@merchantcalico.net>
2025-07-27 19:11:48 +1000

    build: Use Parchment mappings within Fabric build.

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -17,7 +17,10 @@ repositories {
 
 dependencies {
 	minecraft("com.mojang:minecraft:${Versions.MINECRAFT}")
-	mappings(loom.officialMojangMappings())
+	mappings(loom.layered {
+		officialMojangMappings()
+		parchment("org.parchmentmc.data:parchment-${Versions.PARCHMENT_MINECRAFT}:${Versions.PARCHMENT}")
+	})
 
 	modImplementation("net.fabricmc:fabric-loader:${Versions.FABRIC_LOADER}")
 	modImplementation("net.fabricmc.fabric-api:fabric-api:${Versions.FABRIC_API}")
