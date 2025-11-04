15f3a0d3c51d306afbfc39d85f2c1cd331867e8a
sylv256 <contact@sylv.gay>
2025-11-03 22:41:52 -0500

    feat!: set default namespace for Silicate registries to `silicate`

---
+++ common/src/main/java/lgbt/greenhouse/silicate/mixin/Mixin_Registry.java
@@ -0,0 +1,39 @@
+package lgbt.greenhouse.silicate.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.mojang.serialization.Codec;
+import lgbt.greenhouse.silicate.Silicate;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.function.Function;
+
+@Mixin(Registry.class)
+public interface Mixin_Registry<T> {
+	@Shadow
+	ResourceKey<? extends Registry<T>> key();
+
+	@ModifyExpressionValue(
+			method = "referenceHolderWithLifecycle",
+			at = @At(
+					value = "FIELD",
+					target = "Lnet/minecraft/resources/ResourceLocation;CODEC:Lcom/mojang/serialization/Codec;"
+			)
+	)
+	private Codec<ResourceLocation> silicate$overrideDefaultNamespace(Codec<ResourceLocation> original) {
+		if (!this.key().location().getNamespace().equals(Silicate.MOD_ID)) return original;
+
+		Function<ResourceLocation, ResourceLocation> namespaceTransformer = location -> {
+			if (location.getNamespace().equals(ResourceLocation.DEFAULT_NAMESPACE)) {
+				return Silicate.id(location.getPath());
+			} else {
+				return location;
+			}
+		};
+		return original.xmap(namespaceTransformer, namespaceTransformer);
+	}
+}
