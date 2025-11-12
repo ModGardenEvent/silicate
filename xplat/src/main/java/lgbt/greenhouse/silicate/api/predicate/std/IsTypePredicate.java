702c44354f0749f8f96c7511251a236d43174c9b
sylv256 <contact@sylv.gay>
2025-11-12 07:26:25 -0500

    refactor!: `predicate.builtin.IsTypePredicate` -> `predicate.std.IsTypePredicate`

--- xplat/src/main/java/lgbt/greenhouse/silicate/api/predicate/std/IsTypePredicate.java
+++ xplat/src/main/java/lgbt/greenhouse/silicate/api/predicate/std/IsTypePredicate.java
@@ -1,4 +1,4 @@
-package lgbt.greenhouse.silicate.api.predicate.builtin;
+package lgbt.greenhouse.silicate.api.predicate.std;
 
 import com.mojang.serialization.MapCodec;
 import lgbt.greenhouse.silicate.api.predicate.GamePredicate;
